6b323a787eb64ad664160f65938f714d664c314b
KAUAN LUIZ DA SILVA <140724724+kauan-bit@users.noreply.github.com>
2023-10-31 11:04:38 -0300

    Update index.js
    mudança de variáveis e else if

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-
 let name = "kauan";
 let xp= 7500 
      
-let = level 
+let = nivel
 
 if (xp < 10000 ){
     nivel = "'ferro";
-} if else (xp <=1001 && xp <=2000) {
-    nivel= "bronze";
-} if else (xp <=2001 && xp <=5000){
-    nivel="prata";
-} if else (xp <=6001 && xp <= 7000){
-    nvel=" ouro"
+} else if  (xp <=1001 && xp <=2000) {
+    nivel = "bronze";
+} else if  (xp <=2001 && xp <=5000){
+    nivel ="prata";
+} else if(xp <=6001 && xp <= 7000){
+    nivel =" ouro"
 } else if (xp >= 7001 && xp <= 8000) {
     nivel = "Platina";
-  } else if (xp >= 8001 && xp <= 9000) {
+} else if (xp >= 8001 && xp <= 9000) {
     nivel = "Ascendente";
-  } else if (xp >= 9001 && xp <= 10000) {
+} else if (xp >= 9001 && xp <= 10000) {
     nivel = "Imortal";
-  } else {
+} else {
     nivel = "Radia";
   }
   
