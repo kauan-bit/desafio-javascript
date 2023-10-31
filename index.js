c61cf0149f1d99c1c6063f733c5b611b0b787b45
KAUAN LUIZ DA SILVA <140724724+kauan-bit@users.noreply.github.com>
2023-10-31 10:57:24 -0300

    Update and rename first step of project to index.js
    foi necessário mudar o nome do arquivo, pois não estava conseguindo me localizar facilmente

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let name = "kauan";
+let xp= 7500 
+     
+let = level 
+
+if (xp < 10000 ){
+    nivel = "'ferro";
+} if else (xp <=1001 && xp <=2000) {
+    nivel= "bronze";
+} if else (xp <=2001 && xp <=5000){
+    nivel="prata";
+} if else (xp <=6001 && xp <= 7000){
+    nvel=" ouro"
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+  } else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+  } else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+  } else {
+    nivel = "Radia";
+  }
+  
+  console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
